09f03d082e514d12b1bc97a7e655c2d8addf1b16
SERT <SERT>
2019-11-14 17:36:40 -0800

    initial commit

---
+++ src/main/kotlin/org/sert2521/bunnybots2019/Constants.kt
@@ -0,0 +1,13 @@
+package org.sert2521.bunnybots2019
+
+import org.sert2521.bunnybots2019.tubintake.TubIntake
+import org.sert2521.sertain.motors.TalonId
+import org.sert2521.sertain.motors.VictorId
+
+object MotorControllers {
+    val TUBINTAKE_WHEEL_LEFT = TalonId(99999999)
+    val TUBINTAKE_WHEEL_RIGHT = TalonId(99999999)
+
+    val TUBINTAKE_ARM_A = TalonId(999999)
+    val TUBINTAKE_ARM_B = TalonId(999999)
+}
