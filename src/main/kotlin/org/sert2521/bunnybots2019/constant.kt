72d4bdb569cfeadb043abe4114471aca5946674f
Carkoth <danielbrase.carkoth@gmail.com>
2019-11-14 17:37:10 -0800

    Add constants

--- src/main/kotlin/org/sert2521/bunnybots2019/constant.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/constant.kt
@@ -1,2 +1,8 @@
 package org.sert2521.bunnybots2019
 
+import org.sert2521.sertain.motors.TalonId
+
+val rightFront = TalonId(0)
+val rightBack = TalonId(1)
+val leftFront = TalonId(2)
+val leftBack = TalonId(3)
--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
@@ -1,4 +1,8 @@
 package org.sert2521.bunnybots2019.drivetrain
 
-class Drivetrain {
+import org.sert2521.sertain.motors.MotorController
+import org.sert2521.sertain.subsystems.Subsystem
+
+class Drivetrain : Subsystem("Drivetrain") {
+
 }
