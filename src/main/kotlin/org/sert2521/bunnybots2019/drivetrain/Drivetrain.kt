1b2aba3e016347b1835024449ec232d5f528ebe4
Carkoth <kturner44@4j.lane.edu>
2019-11-14 17:56:47 -0800

    Add motors to Drivetrain class

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
@@ -1,8 +1,14 @@
 package org.sert2521.bunnybots2019.drivetrain
 
+import edu.wpi.first.wpilibj.DriverStation
 import org.sert2521.sertain.motors.MotorController
 import org.sert2521.sertain.subsystems.Subsystem
 
 class Drivetrain : Subsystem("Drivetrain") {
-
+    val rightFront = MotorController(org.sert2521.bunnybots2019.rightFront)
+    val rightBack = MotorController(org.sert2521.bunnybots2019.rightBack)
+    val leftFront = MotorController(org.sert2521.bunnybots2019.leftFront)
+    val leftBack = MotorController(org.sert2521.bunnybots2019.leftBack)
 }
+
+//val ds =  DriverStation.getInstance().
