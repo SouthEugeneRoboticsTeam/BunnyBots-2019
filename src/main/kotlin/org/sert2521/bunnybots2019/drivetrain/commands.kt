08dbce12b88d963db2d0b943a1811bb926d1940f
Carkoth <kturner44@4j.lane.edu>
2019-11-14 17:59:39 -0800

    Set up default for commands

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/commands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/commands.kt
@@ -1,2 +1,17 @@
 package org.sert2521.bunnybots2019.drivetrain
 
+import kotlinx.coroutines.cancelAndJoin
+import org.sert2521.bunnybots2019.drivetrain.Drivetrain
+import org.sert2521.sertain.Robot
+import org.sert2521.sertain.coroutines.delayUntil
+import org.sert2521.sertain.events.onTick
+import org.sert2521.sertain.subsystems.doTask
+
+suspend fun Robot.dumpBed(speed: Double) = doTask {
+    val bedDumper = use<Drivetrain>()
+    action {
+        val job = onTick {
+
+        }
+    }
+}
