d373edcc03e94bdcc62e66dd7c48830baf498ef1
Carkoth <kturner44@4j.lane.edu>
2019-11-19 17:29:12 -0800

    add drive function

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
@@ -1,12 +1,19 @@
 package org.sert2521.bunnybots2019.drivetrain
 
 import edu.wpi.first.wpilibj.DriverStation
+import org.sert2521.bunnybots2019.leftBack
 import org.sert2521.sertain.motors.MotorController
 import org.sert2521.sertain.subsystems.Subsystem
+import org.sert2521.bunnybots2019.leftFront
+import org.sert2521.bunnybots2019.rightBack
+import org.sert2521.bunnybots2019.rightFront
 
 class Drivetrain : Subsystem("Drivetrain") {
-    val rightFront = MotorController(org.sert2521.bunnybots2019.rightFront)
-    val rightBack = MotorController(org.sert2521.bunnybots2019.rightBack)
-    val leftFront = MotorController(org.sert2521.bunnybots2019.leftFront)
-    val leftBack = MotorController(org.sert2521.bunnybots2019.leftBack)
+    private val right = MotorController(rightFront, rightBack)
+    private val left = MotorController(leftFront, leftBack)
+
+    fun drive(speed: Double, turn: Double){
+        right.setPercentOutput(speed - turn)
+        left.setPercentOutput(speed + turn)
+    }
 }
--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/commands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/commands.kt
@@ -1,5 +1,6 @@
 package org.sert2521.bunnybots2019.drivetrain
 
+import edu.wpi.first.wpilibj.Preferences
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import kotlinx.coroutines.cancelAndJoin
@@ -10,8 +11,15 @@ import org.sert2521.sertain.coroutines.delayUntil
 import org.sert2521.sertain.events.onTick
 import org.sert2521.sertain.subsystems.doTask
 
-//fun Number.remap(fromRange: DoubleRange, toRange: DoubleRange) =
-//        (this.toDouble() - fromRange.start) * (toRange.endInclusive - toRange.start) / (fromRange.endInclusive - fromRange.start) + toRange.start
+val driveSpeedScalar get() = Preferences.getInstance().getDouble("drive_speed_scalar", 1.0)
+
+typealias DoubleRange = ClosedFloatingPointRange<Double>
+
+fun DoubleRange.intersects(other: DoubleRange): Boolean =
+        start in other || endInclusive in other
+
+fun Number.remap(fromRange: DoubleRange, toRange: DoubleRange) =
+        (this.toDouble() - fromRange.start) * (toRange.endInclusive - toRange.start) / (fromRange.endInclusive - fromRange.start) + toRange.start
 
 val primaryJoystick by lazy { Joystick(Operator.PRIMARY_STICK) }
 
@@ -20,15 +28,17 @@ private val turn get() = primaryJoystick.x
 private val scale get() = 1.0
 
 suspend fun Robot.driveTrain(speed: Double) = doTask {
-    val bedDumper = use<Drivetrain>()
+    val drivetrain = use<Drivetrain>()
     action {
         val job = onTick {
 
-//            val throttleScalar = scale.remap(0.0..1.0, 0.5..1.0)
-//            val turnScalar = scale.remap(0.0..1.0, 0.4..1.0)
+            val throttleScalar = scale.remap(0.0..1.0, 0.5..1.0)
+            val turnScalar = scale.remap(0.0..1.0, 0.4..1.0)
 
-//            val scaledThrottle = throttle * throttleScalar * liftScalar * -driveSpeedScalar
-//            val scaledTurn = turn * turnScalar * liftScalar * driveSpeedScalar
+            val scaledThrottle = throttle * throttleScalar * -driveSpeedScalar
+            val scaledTurn = turn * turnScalar * driveSpeedScalar
+
+            drivetrain.drive(scaledThrottle, scaledTurn)
         }
     }
 }
