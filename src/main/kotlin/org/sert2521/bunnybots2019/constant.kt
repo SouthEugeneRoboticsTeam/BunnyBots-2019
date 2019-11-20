783d41f8d08b4b8691d36f01ef4243f503ecafa4
Carkoth <kturner44@4j.lane.edu>
2019-11-19 17:00:52 -0800

    started joystick stuff

--- src/main/kotlin/org/sert2521/bunnybots2019/constant.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/constant.kt
@@ -6,3 +6,9 @@ val rightFront = TalonId(0)
 val rightBack = TalonId(1)
 val leftFront = TalonId(2)
 val leftBack = TalonId(3)
+
+object Operator {
+    const val PRIMARY_CONTROLLER = 0
+    const val PRIMARY_STICK = 2
+    const val SECONDARY_STICK = 1
+}
--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/commands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/commands.kt
@@ -1,17 +1,34 @@
 package org.sert2521.bunnybots2019.drivetrain
 
+import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import kotlinx.coroutines.cancelAndJoin
+import org.sert2521.bunnybots2019.Operator
 import org.sert2521.bunnybots2019.drivetrain.Drivetrain
 import org.sert2521.sertain.Robot
 import org.sert2521.sertain.coroutines.delayUntil
 import org.sert2521.sertain.events.onTick
 import org.sert2521.sertain.subsystems.doTask
 
-suspend fun Robot.dumpBed(speed: Double) = doTask {
+//fun Number.remap(fromRange: DoubleRange, toRange: DoubleRange) =
+//        (this.toDouble() - fromRange.start) * (toRange.endInclusive - toRange.start) / (fromRange.endInclusive - fromRange.start) + toRange.start
+
+val primaryJoystick by lazy { Joystick(Operator.PRIMARY_STICK) }
+
+private val throttle get() = primaryJoystick.y
+private val turn get() = primaryJoystick.x
+private val scale get() = 1.0
+
+suspend fun Robot.driveTrain(speed: Double) = doTask {
     val bedDumper = use<Drivetrain>()
     action {
         val job = onTick {
 
+//            val throttleScalar = scale.remap(0.0..1.0, 0.5..1.0)
+//            val turnScalar = scale.remap(0.0..1.0, 0.4..1.0)
+
+//            val scaledThrottle = throttle * throttleScalar * liftScalar * -driveSpeedScalar
+//            val scaledTurn = turn * turnScalar * liftScalar * driveSpeedScalar
         }
     }
 }
