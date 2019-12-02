6e5a5e035a98ed1ceaca8bdb149c84ce8b68d857
solidstatebird <33707046+circuitbird@users.noreply.github.com>
2019-12-01 22:16:34 -0800

    wip

--- src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeCommands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeCommands.kt
@@ -1,2 +1,16 @@
 package org.sert2521.bunnybots2019.tubintake
 
+import org.sert2521.sertain.Robot
+import org.sert2521.sertain.robot
+import org.sert2521.sertain.subsystems.doTask
+
+fun main() = robot {
+    add<TubIntake>()
+}
+
+suspend fun Robot.tubIntakeControl() = doTask {
+    val intake = use<TubIntake>()
+    action {
+
+    }
+}
--- src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeConstants.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeConstants.kt
@@ -1,2 +1,7 @@
 package org.sert2521.bunnybots2019.tubintake
 
+//both should be positive
+const val INTAKE_SPEED = .3
+const val OUTTAKE_SPEED = .5
+
+const val ENCODER_TICKS = 2048
