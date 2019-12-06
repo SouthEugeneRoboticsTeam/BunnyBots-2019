59dd19f10ecb9bfa495fc0a86e05320840682b33
Carkoth <kturner44@4j.lane.edu>
2019-12-05 17:20:05 -0800

    add main kt drive

--- src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
@@ -2,6 +2,7 @@ package org.sert2521.bunnybots2019
 
 import kotlinx.coroutines.launch
 import org.sert2521.bunnybots2019.drivetrain.Drivetrain
+import org.sert2521.bunnybots2019.drivetrain.driveTrain
 import org.sert2521.sertain.events.onEnable
 import org.sert2521.sertain.events.onTeleop
 import org.sert2521.sertain.robot
@@ -11,12 +12,6 @@ fun main() = robot {
     add<Drivetrain>()
 
     onTeleop {
-        doTask {
-            val dt = use<Drivetrain>()
-            action {
-                dt.arcadeDrive(.5, 0.0)
-            }
-        }
+        driveTrain()
     }
-
 }
