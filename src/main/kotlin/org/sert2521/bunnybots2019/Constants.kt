f677f2f3308b14a36f6b51353476f69c59a61299
Aidan Heller <asfheller@gmail.com>
2019-12-05 19:40:22 -0800

    Clean up code

--- src/main/kotlin/org/sert2521/bunnybots2019/Constants.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/Constants.kt
@@ -2,13 +2,13 @@ package org.sert2521.bunnybots2019
 
 import org.sert2521.sertain.motors.TalonId
 
+// Drivetrain motor ids
 val rightFront = TalonId(3)
 val rightBack = TalonId(4)
 val leftFront = TalonId(1)
 val leftBack = TalonId(2)
 
-object Operator {
-    const val PRIMARY_CONTROLLER = 0
-    const val PRIMARY_STICK = 2
-    const val SECONDARY_STICK = 1
-}
+// Operator constants
+const val PRIMARY_CONTROLLER = 0
+const val PRIMARY_STICK = 2
+const val SECONDARY_STICK = 1
--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/DrivetrainCommands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/DrivetrainCommands.kt
@@ -1,19 +1,10 @@
 package org.sert2521.bunnybots2019.drivetrain
 
-import edu.wpi.first.wpilibj.DriverStation
-import edu.wpi.first.wpilibj.Preferences
-import edu.wpi.first.wpilibj.Joystick
-import org.sert2521.bunnybots2019.Operator
+import org.sert2521.bunnybots2019.oi.primaryJoystick
 import org.sert2521.sertain.events.onTick
-import org.sert2521.sertain.input.stickAxis
-import org.sert2521.sertain.input.stickButton
 import org.sert2521.sertain.subsystems.doTask
 import org.sert2521.sertain.subsystems.use
 import kotlin.math.sign
-
-val driveSpeedScalar get() = Preferences.getInstance().getDouble("drive_speed_scalar", 1.0)
-
-typealias DoubleRange = ClosedFloatingPointRange<Double>
 
 fun Double.deadband(range: Double): Double{
     return if(this < range && this > -range) {
@@ -23,17 +14,8 @@ fun Double.deadband(range: Double): Double{
     }
 }
 
-fun DoubleRange.intersects(other: DoubleRange): Boolean =
-        start in other || endInclusive in other
-
-fun Number.remap(fromRange: DoubleRange, toRange: DoubleRange) =
-        (this.toDouble() - fromRange.start) * (toRange.endInclusive - toRange.start) / (fromRange.endInclusive - fromRange.start) + toRange.start
-
-val primaryJoystick by lazy { Joystick(Operator.PRIMARY_STICK) }
-
 private val throttle get() = primaryJoystick.y
 private val turn get() = primaryJoystick.x
-private val scale get() = 1.0
 
 suspend fun driveTrain() = doTask {
     val drivetrain = use<Drivetrain>()
