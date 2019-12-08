3fb2b48960ecfad005382da78e139586e460d66d
solidstatebird <33707046+solidstatebird@users.noreply.github.com>
2019-12-07 22:20:24 -0800

    remove unnecessary constants

--- src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeConstants.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeConstants.kt
@@ -8,10 +8,8 @@ const val ARM_KP = 0.1
 const val ARM_KI = 0.0
 const val ARM_KD = 0.0
 
+//CORRECT THESE VALUES BEFORE USING THE ARM!!!
 const val ENCODER_TICKS = 2048
-
-const val ARM_UP = 0
-const val ARM_DOWN = 1
 
 const val ARM_UP_TICKS = 0
 const val ARM_DOWN_TICKS = 10000
