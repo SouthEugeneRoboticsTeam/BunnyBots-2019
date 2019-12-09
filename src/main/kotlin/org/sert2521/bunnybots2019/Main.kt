d6f08f33963a24832d440965a3793131030f6977
Aidan Heller <asfheller@gmail.com>
2019-12-08 17:55:15 -0800

    Add network tables

--- src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
@@ -1,9 +1,19 @@
 package org.sert2521.bunnybots2019
 
+import kotlinx.coroutines.delay
 import org.sert2521.bunnybots2019.drivetrain.Drivetrain
+import org.sert2521.bunnybots2019.oi.WatashiNoTable
+import org.sert2521.sertain.events.onEnable
 import org.sert2521.sertain.robot
 import org.sert2521.sertain.subsystems.add
 
 suspend fun main() = robot {
     add<Drivetrain>()
+    onEnable {
+        WatashiNoTable.x
+        delay(1000)
+        WatashiNoTable.x = 4
+        delay(10000)
+        println(WatashiNoTable.x)
+    }
 }
--- src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
@@ -2,5 +2,10 @@ package org.sert2521.bunnybots2019.oi
 
 import edu.wpi.first.wpilibj.Joystick
 import org.sert2521.bunnybots2019.PRIMARY_STICK
+import org.sert2521.sertain.networktables.Table
 
 val primaryJoystick by lazy { Joystick(PRIMARY_STICK) }
+
+object WatashiNoTable : Table("WatashiNoTable") {
+    var x by entry(5)
+}
