8f7eba2498dd235dd213f35e9177a7a6186e773a
William <whou125@gmail.com>
2019-12-12 19:36:30 -0800

    Get joystick/controller inputs

--- src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
@@ -1,6 +1,11 @@
 package org.sert2521.bunnybots2019.oi
 
 import edu.wpi.first.wpilibj.Joystick
+import kotlinx.coroutines.CoroutineScope
 import org.sert2521.bunnybots2019.PRIMARY_STICK
 
 val primaryJoystick by lazy { Joystick(PRIMARY_STICK) }
+
+fun CoroutineScope.getInputs() {
+
+}
