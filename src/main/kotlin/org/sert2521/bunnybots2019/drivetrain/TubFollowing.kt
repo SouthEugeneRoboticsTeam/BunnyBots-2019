49f83f5cbbdc23dd2782700a761f32a297a59435
Carkoth <kturner44@4j.lane.edu>
2019-12-13 16:41:24 -0800

    made it turn slower if closet to facing the target

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/TubFollowing.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/TubFollowing.kt
@@ -1,6 +1,8 @@
 package org.sert2521.bunnybots2019.drivetrain
 
 import edu.wpi.first.networktables.NetworkTableInstance
+import java.lang.Math.pow
+import kotlin.math.pow
 
 private suspend fun pickUpTub (drivetrain: Drivetrain, turnSpeed: Double, activationNum: Double, distanceToSlow: Double){
     var table = NetworkTableInstance.getDefault().getTable("Vision")
@@ -9,8 +11,8 @@ private suspend fun pickUpTub (drivetrain: Drivetrain, turnSpeed: Double, activa
     var xAngle = table.getEntry("x_angle_from_center").toString().toDouble()
 
     if(distance > distanceToSlow) {
-        drivetrain.arcadeDrive(1.0, xAngle.deadband(activationNum) * turnSpeed)
+        drivetrain.arcadeDrive(1.0, xAngle.deadband(activationNum).pow(2) * turnSpeed.pow(2))
     }else{
-        drivetrain.arcadeDrive(0.25, xAngle.deadband(activationNum) * turnSpeed)
+        drivetrain.arcadeDrive(0.25, xAngle.deadband(activationNum) * turnSpeed.pow(2))
     }
 }
