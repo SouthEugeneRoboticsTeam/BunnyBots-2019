75ab8c29ea4b46c9d35f7167b1d4e18f29dd9cee
William Hou <whou125@gmail.com>
2019-12-14 17:44:59 -0800

    Apply suggestions from code review
    Co-Authored-By: Andrew Dassonville <dassonville.andrew@gmail.com>

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
@@ -8,7 +8,7 @@ import org.sert2521.sertain.motors.MotorController
 import org.sert2521.sertain.subsystems.Subsystem
 
 class Drivetrain : Subsystem("Drivetrain", ::controlDrivetrain) {
-    private val right = MotorController(rightFront, rightBack){
+    private val right = MotorController(rightFront, rightBack) {
         inverted = true
         brakeMode = true
     }
