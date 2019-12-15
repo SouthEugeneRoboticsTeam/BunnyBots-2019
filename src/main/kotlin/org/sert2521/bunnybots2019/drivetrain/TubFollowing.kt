8d30f8ce9259d5c3a5754424401423a58d35c5f2
Carkoth <kturner44@4j.lane.edu>
2019-12-15 14:56:22 -0800

    last alive

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/TubFollowing.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/TubFollowing.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.cancel
 import org.sert2521.sertain.events.onTick
 import org.sert2521.sertain.subsystems.doTask
 import org.sert2521.sertain.subsystems.use
+import java.time.Instant
 import kotlin.math.pow
 
 suspend fun pickUpTub(turnSpeed: Double, activationNum: Double, distanceToSlow: Double, distanceToFinish: Double) = doTask {
@@ -17,12 +18,15 @@ suspend fun pickUpTub(turnSpeed: Double, activationNum: Double, distanceToSlow: 
 
             var distance = table.getEntry("distance").getDouble(200.0)
             var xAngle = table.getEntry("x_angle_from_center").getDouble(0.0)
+            var lastAlive = table.getEntry("x_angle_from_center").getDouble(0.0)
 
-            when {
-                distance > distanceToSlow -> drivetrain.arcadeDrive(0.5, (xAngle.deadband(activationNum) * turnSpeed).pow(3))
-                distanceToSlow >= distance && distance > distanceToFinish -> drivetrain.arcadeDrive(0.25, (xAngle.deadband(activationNum) * turnSpeed).pow(3))
-                else -> {
-                    this@action.cancel()
+            if (lastAlive - (Instant.now().toEpochMilli() / 1000.0) < 1.0) {
+                when {
+                    distance > distanceToSlow -> drivetrain.arcadeDrive(0.1, (xAngle.deadband(activationNum) * turnSpeed).pow(3))
+                    distanceToSlow >= distance && distance > distanceToFinish -> drivetrain.arcadeDrive(0.02, (xAngle.deadband(activationNum) * turnSpeed).pow(3))
+                    else -> {
+                        this@action.cancel()
+                    }
                 }
             }
         }
