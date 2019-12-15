47e3cd079b1bbc118904d6eaa72b921402c53fb3
William <whou125@gmail.com>
2019-12-14 16:34:09 -0800

    Add sendable chooser

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/DrivetrainCommands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/DrivetrainCommands.kt
@@ -2,6 +2,7 @@ package org.sert2521.bunnybots2019.drivetrain
 
 import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.XboxController
+import org.sert2521.bunnybots2019.oi.controlMode
 import org.sert2521.sertain.events.onTick
 import org.sert2521.sertain.subsystems.doTask
 import org.sert2521.sertain.subsystems.use
@@ -9,10 +10,10 @@ import kotlin.math.sign
 
 typealias DoubleRange = ClosedFloatingPointRange<Double>
 
-fun Double.deadband(range: Double): Double{
-    return if(this < range && this > -range) {
+fun Double.deadband(range: Double): Double {
+    return if (this < range && this > -range) {
         0.0
-    }else{
+    } else {
         this
     }
 }
@@ -37,9 +38,9 @@ suspend fun driveTrain() = doTask {
 
             throttle.deadband(0.05)
             turn.deadband(0.05)
-
+            println("ControlMode ${controlMode}")
             val scaledThrottle = -throttle.sign * (throttle * throttle * scale)
-            val scaledTurn = turn.sign * (turn  * turn * scale)
+            val scaledTurn = turn.sign * (turn * turn * scale)
             drivetrain.arcadeDrive(scaledThrottle, scaledTurn)
             println(scaledThrottle)
             println(scaledTurn)
--- src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
@@ -1,11 +1,24 @@
 package org.sert2521.bunnybots2019.oi
 
 import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import kotlinx.coroutines.CoroutineScope
 import org.sert2521.bunnybots2019.PRIMARY_STICK
+
+enum class ControlMode {
+    CONTROLLER, JOYSTICK
+}
+
+val controlModeChooser = SendableChooser<ControlMode>().apply {
+    addOption("Joystick", ControlMode.JOYSTICK)
+    addOption("Controller", ControlMode.CONTROLLER)
+}
+
+val controlMode get() = controlModeChooser.selected ?: ControlMode.CONTROLLER
 
 val primaryJoystick by lazy { Joystick(PRIMARY_STICK) }
 
 fun CoroutineScope.getInputs() {
-
+    SmartDashboard.putData("Control Mode", controlModeChooser)
 }
