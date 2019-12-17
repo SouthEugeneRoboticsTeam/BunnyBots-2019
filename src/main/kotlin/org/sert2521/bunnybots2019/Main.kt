60b31951bd53870dd5852f5dc39f277ee5e738ec
SERT <SERT>
2019-12-16 16:37:53 -0800

    auto commands for tub intake

--- src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
@@ -1,13 +1,7 @@
 package org.sert2521.bunnybots2019
 
-import edu.wpi.first.wpilibj.DigitalInput
-import kotlinx.coroutines.delay
 import org.sert2521.bunnybots2019.tubintake.*
-import org.sert2521.sertain.Robot
-import org.sert2521.sertain.coroutines.delayForever
-import org.sert2521.sertain.events.onAuto
 import org.sert2521.sertain.events.onTeleop
-import org.sert2521.sertain.events.onTick
 import org.sert2521.bunnybots2019.drivetrain.Drivetrain
 import org.sert2521.bunnybots2019.oi.getInputs
 import org.sert2521.sertain.robot
--- src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeCommands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/tubintake/TubIntakeCommands.kt
@@ -1,9 +1,6 @@
 package org.sert2521.bunnybots2019.tubintake
 
-import kotlinx.coroutines.launch
 import org.sert2521.bunnybots2019.oi.Controls
-import org.sert2521.bunnybots2019.utils.timer
-import org.sert2521.sertain.coroutines.RobotScope
 import org.sert2521.sertain.events.onTick
 import org.sert2521.sertain.subsystems.doTask
 import org.sert2521.sertain.subsystems.use
@@ -11,7 +8,7 @@ import org.sert2521.sertain.subsystems.use
 suspend fun teleopIntakeControl() = doTask {
     val intake = use<TubIntake>()
     action {
-        val job = onTick {
+        onTick {
             if (Controls.tubintakeInButton) {
                 intake.spinIntake()
             } else if (Controls.tubintakeOutButton) {
@@ -31,16 +28,26 @@ suspend fun teleopIntakeControl() = doTask {
     }
 }
 
-suspend fun autoIntakeCommand(position: Int, intakeRunning: Boolean) = doTask {
+suspend fun autoRunArm(position: Int) = doTask {
     val intake = use<TubIntake>()
     action {
-        if (intakeRunning) {
-            RobotScope.launch {
-                timer(20, 3000) {
+        intake.runArmToPosition(position)
+    }
+}
+
+suspend fun autoSpinIntake(enabled: Boolean, intaking: Boolean) = doTask {
+    val intake = use<TubIntake>()
+    action {
+        if(!enabled) {
+            intake.stopSpin()
+        } else {
+            onTick {
+                if (intaking) {
                     intake.spinIntake()
+                } else {
+                    intake.spinOuttake()
                 }
             }
         }
-        intake.runArmToPosition(position)
     }
 }
