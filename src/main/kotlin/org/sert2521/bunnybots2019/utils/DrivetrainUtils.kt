baf212f8dd150af5154d413ec4922487a3e6dc4a
Aidan Heller <asfheller@gmail.com>
2019-12-16 19:12:22 -0800

    Clean up

--- src/main/kotlin/org/sert2521/bunnybots2019/utils/DrivetrainUtils.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/utils/DrivetrainUtils.kt
@@ -3,7 +3,6 @@ package org.sert2521.bunnybots2019.utils
 fun Double.deadband(range: Double) =
         if (this < range && this > -range) 0.0 else this
 
-
 fun ClosedRange<Double>.intersects(other: ClosedRange<Double>): Boolean =
         start in other || endInclusive in other
 
