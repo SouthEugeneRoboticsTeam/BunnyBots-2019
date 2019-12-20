ce2f3956ceadca48e9a22602da7359caccaeda18
Aidan Heller <asfheller@gmail.com>
2019-12-20 10:00:31 -0800

    Finalize stuff

--- src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
@@ -6,9 +6,6 @@ import org.sert2521.bunnybots2019.drivetrain.Drivetrain
 import org.sert2521.bunnybots2019.oi.initControls
 import org.sert2521.sertain.events.whileTeleop
 import org.sert2521.bunnybots2019.cubeintake.CubeIntake
-import org.sert2521.bunnybots2019.tubintake.*
-import org.sert2521.bunnybots2019.drivetrain.Drivetrain
-import org.sert2521.bunnybots2019.oi.getInputs
 import org.sert2521.sertain.events.onTeleop
 import org.sert2521.sertain.robot
 import org.sert2521.sertain.subsystems.add
@@ -22,8 +19,5 @@ suspend fun main() = robot {
     whileTeleop {
         initControls()
         teleopIntakeControl()
-    }
-    onTeleop {
-        getInputs()
     }
 }
--- src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/oi/Oi.kt
@@ -10,9 +10,8 @@ import org.sert2521.bunnybots2019.beddumper.dumpBed
 import org.sert2521.sertain.coroutines.watch
 import org.sert2521.bunnybots2019.cubeintake.intakeCubes
 import org.sert2521.bunnybots2019.cubeintake.outtakeCubes
-import org.sert2521.sertain.coroutines.watch
-import org.sert2521.bunnybots2019.tubintake.tubIntake
-import org.sert2521.bunnybots2019.tubintake.tubOuttake
+import org.sert2521.bunnybots2019.tubintake.intakeTub
+import org.sert2521.bunnybots2019.tubintake.outtakeTub
 
 enum class ControlMode {
     CONTROLLER, JOYSTICK
@@ -28,7 +27,7 @@ val controlMode get() = controlModeChooser.selected ?: ControlMode.CONTROLLER
 val primaryJoystick by lazy { Joystick(Operator.PRIMARY_STICK) }
 val primaryController by lazy { XboxController(Operator.PRIMARY_CONTROLLER) }
 
-fun CoroutineScope.getInputs() {
+fun CoroutineScope.initControls() {
     SmartDashboard.putData("Control Mode", controlModeChooser);
 
     { primaryJoystick.getRawButton(3) }.watch {
@@ -44,15 +43,13 @@ fun CoroutineScope.getInputs() {
     { primaryJoystick.getRawButton(Operator.TUBINTAKE_IN_BUTTON) }.watch() {
         whileTrue {
             println("Intake should be spinning in")
-            tubIntake
-        }
-    }
+            intakeTub()
         }
     };
     { primaryJoystick.getRawButton(Operator.TUBINTAKE_OUT_BUTTON) }.watch() {
         whileTrue {
             println("Outtake should be running")
-            tubOuttake()
+            outtakeTub()
         }
     };
     { primaryJoystick.getRawButton(14) }.watch {
