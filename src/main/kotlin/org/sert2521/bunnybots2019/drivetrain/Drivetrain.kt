ba9c2c005336a0d8d592dfccaf5daff96c9d04d0
William Hou <whou125@gmail.com>
2019-12-20 19:49:36 -0800

    Drive distance (#17)
    * Add function to drive straight for a certain distance
    
    * Make drivetrain work with new version of SERTain
    
    * Merge branch 'development' of C:\Users\Public\Robotics\Bunnybots-2019 with conflicts.
    
    * Add distance function using motor controller setpoints
    
    * SERtain changes
    
    Signed-off-by: William <whou125@gmail.com>
    
    * Add motion curve
    
    Co-authored-by: Aidan Heller <asfheller@gmail.com>

--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/Drivetrain.kt
@@ -8,14 +8,30 @@ class Drivetrain : Subsystem("Drivetrain", ::controlDrivetrain) {
     private val right = MotorController(MotorControllers.rightFront, MotorControllers.rightBack) {
         inverted = true
         brakeMode = true
+        sensorInverted = true
+        pidf {
+            kp = 0.0
+            ki = 0.0
+            kd = 0.0
+        }
     }
     private val left = MotorController(MotorControllers.leftFront, MotorControllers.leftBack) {
         brakeMode = true
+        sensorInverted = true
+        pidf {
+            kp = 0.0
+            ki = 0.0
+            kd = 0.0
+        }
     }
 
+    val rightPosition get() = right.position
+    val leftPosition get() = left.position
+    val position get() = (rightPosition + leftPosition) / 2
+
+
     init {
-        right.position = 0
-        left.position = 0
+        zeroEncoders()
     }
 
     fun arcadeDrive(speed: Double, turn: Double) {
@@ -27,4 +43,20 @@ class Drivetrain : Subsystem("Drivetrain", ::controlDrivetrain) {
         right.setPercentOutput(rightSpeed)
         left.setPercentOutput(leftSpeed)
     }
+
+    fun driveToPosition(targetPosition: Int) {
+        right.setTargetPosition(targetPosition)
+        left.setTargetPosition(targetPosition)
+    }
+
+    fun stop() {
+        right.setPercentOutput(0.0)
+        left.setPercentOutput(0.0)
+    }
+
+    fun zeroEncoders() {
+        right.position = 0
+        left.position = 0
+    }
+
 }
--- src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/DrivetrainCommands.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/drivetrain/DrivetrainCommands.kt
@@ -1,16 +1,32 @@
 package org.sert2521.bunnybots2019.drivetrain
 
 import edu.wpi.first.wpilibj.GenericHID
+import kotlinx.coroutines.cancel
 import org.sert2521.bunnybots2019.oi.primaryJoystick
 import org.sert2521.bunnybots2019.utils.deadband
 import org.sert2521.bunnybots2019.oi.ControlMode
 import org.sert2521.bunnybots2019.oi.controlMode
 import org.sert2521.bunnybots2019.oi.primaryController
+import org.sert2521.sertain.control.MotionCurve
 import org.sert2521.sertain.events.onTick
+import org.sert2521.sertain.motors.EncoderTicks
 import org.sert2521.sertain.subsystems.doTask
 import org.sert2521.sertain.subsystems.use
+import org.sert2521.sertain.units.Chronic
+import org.sert2521.sertain.units.CompositeUnit
+import org.sert2521.sertain.units.CompositeUnitType
+import org.sert2521.sertain.units.Linear
+import org.sert2521.sertain.units.LinearUnit
+import org.sert2521.sertain.units.Meters
+import org.sert2521.sertain.units.MetricUnit
+import org.sert2521.sertain.units.MetricValue
+import org.sert2521.sertain.units.Per
+import org.sert2521.sertain.units.Radians
+import org.sert2521.sertain.units.Seconds
+import org.sert2521.sertain.units.convertTo
+import org.sert2521.sertain.units.div
+import org.sert2521.sertain.utils.timer
 import kotlin.math.sign
-
 
 private val throttle
     get() = when (controlMode) {
@@ -30,6 +46,47 @@ suspend fun controlDrivetrain() = doTask {
             val scaledThrottle = (-throttle.sign * (throttle * throttle)).deadband(.05)
             val scaledTurn = (turn.sign * (turn * turn)).deadband(.05)
             drivetrain.arcadeDrive(scaledThrottle, scaledTurn)
+        }
+    }
+}
+
+// Drives the robot in a straight line for a certain distance (in inches) at a given speed
+suspend fun driveCurve(speed: Double, distance: Double) = doTask {
+    val drivetrain = use<Drivetrain>()
+    action {
+        drivetrain.zeroEncoders()
+        onTick {
+            drivetrain.arcadeDrive(speed, 0.0)
+            // Calculates the number of encoder pulses corresponding to a certain distance
+            val ticks = (PULSES_PER_REVOLUTION * distance / 2.0) / (2 * Math.PI * WHEEL_RADIUS)
+            if (drivetrain.position >= ticks) {
+                drivetrain.stop()
+                this@action.cancel()
+            }
+        }
+    }
+}
+
+suspend fun driveCurve(
+    speed: MetricValue<CompositeUnitType<Per, Linear, Chronic>, CompositeUnit<Per, Linear, Chronic>>,
+    distance: MetricValue<Linear, MetricUnit<Linear>>
+) = doTask {
+    val drivetrain = use<Drivetrain>()
+    action {
+        drivetrain.zeroEncoders()
+
+        val ticks = MetricValue(
+            Radians,
+            distance.convertTo(Meters).value / WHEEL_RADIUS_METERS
+        ).convertTo(EncoderTicks(PULSES_PER_REVOLUTION.toInt())).value
+        val ticksPerSecond = MetricValue(
+                Radians / Seconds,
+                speed.convertTo(Meters / Seconds).value / WHEEL_RADIUS_METERS
+        ).convertTo(EncoderTicks(PULSES_PER_REVOLUTION.toInt()) / Seconds).value
+        val curve = MotionCurve(ticks, ticksPerSecond, 100.0, 100.0)
+
+        timer(20, 0, (curve.t7 * 1000).toLong()) {
+            drivetrain.driveToPosition(curve.d(it / 1000.0).toInt())
         }
     }
 }
