18bc15bb3e6780fb6c5fda57f86d0d2ee47d3aeb
Aidan Heller <asfheller@gmail.com>
2019-12-21 10:40:56 -0800

    Auto... ?

--- src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
+++ src/main/kotlin/org/sert2521/bunnybots2019/Main.kt
@@ -9,9 +9,13 @@ import org.sert2521.bunnybots2019.drivetrain.Drivetrain
 import org.sert2521.bunnybots2019.oi.initControls
 import org.sert2521.sertain.events.whileTeleop
 import org.sert2521.bunnybots2019.cubeintake.CubeIntake
+import org.sert2521.bunnybots2019.drivetrain.driveCurve
 import org.sert2521.bunnybots2019.oi.controlModeChooser
+import org.sert2521.sertain.events.onAuto
 import org.sert2521.sertain.robot
 import org.sert2521.sertain.subsystems.add
+import org.sert2521.sertain.units.m
+import org.sert2521.sertain.units.mps
 
 suspend fun main() = robot {
     add<TubIntake>()
@@ -22,6 +26,10 @@ suspend fun main() = robot {
     launch {
         delay(1000)
         SmartDashboard.putData("Control Mode", controlModeChooser);
+    }
+
+    onAuto {
+        driveCurve(2.mps, 1.m)
     }
 
     whileTeleop {
